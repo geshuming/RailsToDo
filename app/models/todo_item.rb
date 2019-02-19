39d5ac69deb3d332795bd67550a6204a3ddd3d6a
Shuming <geshuming2@gmail.com>
2019-02-19 17:31:44 +0800

    Added styling to highlight todo_items that are due soon

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -4,4 +4,8 @@ class TodoItem < ApplicationRecord
   def completed?
     !completed_at.blank?
   end
+
+  def due_soon
+    return due_at < Time.now ? 2 : due_at < (Time.now + 1.days) ? 1 : 0
+  end
 end
